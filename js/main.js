25f7cb751f998b1ae63aa63df3f7fadda823278c
sarvar-bakhadirovic <sarvarxalimbetov60@gmail.com>
2022-12-14 21:20:04 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const _addForm = document.getElementById('add-form');
 const _editForm = document.getElementById('edit-form');
 const _closeBtn = document.getElementById('close-btn');
 const _editModal = document.getElementById('edit-student-modal')
+const _addDataModal = document.getElementById('add-student-modal')
+const _status = document.getElementById('status')
+const _count = document.querySelector('.count')
 
 let date = new Date()
 const realDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
@@ -43,14 +46,14 @@ function countMark(mark) {
 
 function addData(info) {
 
-    return info.forEach(item => {
+    return info.forEach((item) => {
         _list.innerHTML += `
         <li class="item">
             <span class="number">${item.id}</span>
             <h1 class="title">${item.name} ${item.lastName}</h1>
             <p class="date">${realDate}</p>
             <span class="mark">${countMark(item.mark)}</span>
-            <p class="status" id="status">${countMark(item.mark) > 40 ? 'Pass' : 'Fail'}</p>
+            <p class="status" id="status">${item.mark >= 90 ? 'Pass' : 'Fail'}</p>
             <button class="edit" onclick="editData(${item.id})">
                 <i class="fa-solid fa-pen"></i>
             </button>
@@ -58,9 +61,61 @@ function addData(info) {
                 <i class="fa-solid fa-trash"></i>
             </button>
         </li>
+        `
+    })
+
+}
+
+// add
+async function createData(data) {
+
+    const response = await fetch(API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(data => data.json())
+    .then(data => {
+        _list.innerHTML += `
+        <li class="item">
+            <span class="number">${data.id}</span>
+            <h1 class="title">${data.name} ${data.lastName}</h1>
+            <p class="date">${realDate}</p>
+            <span class="mark">${countMark(data.mark)}</span>
+            <p class="status" id="status">${countMark(data.mark) > 40 ? 'Pass' : 'Fail'}</p>
+            <button class="edit" onclick="editData(${data.id})">
+                <i class="fa-solid fa-pen"></i>
+            </button>
+            <button class="del" onclick="deleteData(${data.id})">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </li>
     `
     })
 }
+
+_addForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let {
+        name,
+        lastname,
+        mark
+    } = e.target.elements
+
+    let newData = {
+        name: name.value,
+        lastName: lastname.value,
+        markedDate: realDate,
+        mark: mark.value,
+    }
+    createData(newData)
+    
+    _addDataModal.style.display = 'none'
+    document.querySelector('.modal-backdrop').style.display = 'none'
+})
 
 
 // delete
