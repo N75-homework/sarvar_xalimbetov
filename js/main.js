5db03b36f1542d1a1744ebe5bf388c444739c8dc
sarvar-bakhadirovic <sarvarxalimbetov60@gmail.com>
2022-12-18 15:07:29 +0500

    edit bo'limi ham tugatildi

--- js/main.js
+++ js/main.js
@@ -2,61 +2,60 @@ const API = 'http://167.235.158.238/students';
 const _list = document.getElementById('list');
 const _error = document.getElementById('error');
 const _addForm = document.getElementById('add-form');
-const _editForm = document.getElementById('edit-form');
-const _closeBtn = document.getElementById('close-btn');
-const _editModal = document.getElementById('edit-student-modal')
 const _addDataModal = document.getElementById('add-student-modal')
+const _editForm = document.getElementById('edit-form');
+const _editModal = document.getElementById('edit-student-modal')
+const _closeBtn = document.getElementById('close-btn');
 const _status = document.getElementById('status')
 const _count = document.querySelector('.count')
 const _totalMark = document.querySelector('.text-end')
 
 let date = new Date()
 const realDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
-const HIGHEST_SCORE = 150
+const HIGHEST_SCORE = 100
 
 
-
+// fetch
 async function data() {
 
     try {
         const response = await fetch(API)
         const body = await response.json()
-        return body
+        addData(await body)
+        return await body
     } catch (error) {
         addData(students)
     }
 }
-
 data()
-    .then((data) => addData(data) )
-    .catch((error) => {
-        addData(students)
-    })
 
 
+
+// count
 function countMark(mark) {
 
-    if (mark && mark <= 150) {
-        const result = (Number(mark) * 100) / 150
+    if (mark && mark <= 100) {
+        const result = (Number(mark) * 100) / HIGHEST_SCORE
         return `${result.toFixed(0)}%`
-    } else {
-        return 'NaN'
+    } else if(mark && mark > 100){
+        return `100%`
     }
 }
 
+//render
 function addData(info) {
     _count.innerHTML = `Count: ${info.length}`
+    _list.innerHTML = null
 
-
-    return info.forEach((item) => {
+    return info.forEach((item, id) => {
         _list.innerHTML += `
         <li class="item">
             <span class="number">${item.id}</span>
             <h1 class="title">${item.name} ${item.lastName}</h1>
             <p class="date">${realDate}</p>
             <span class="mark">${countMark(item.mark)}</span>
-            <p class="status" id="status">${item.mark > 90 ? 'Pass' : 'Fail'}</p>
-            <button class="edit" onclick="editData(${item.id})">
+            <p class="status" id="status">${item.mark > 59 ? 'Pass' : 'Fail'}</p>
+            <button class="edit" onclick="editData(${item.id}, ${id})">
                 <i class="fa-solid fa-pen"></i>
             </button>
             <button class="del" onclick="deleteData(${item.id})">
@@ -71,7 +70,7 @@ function addData(info) {
 // add
 async function createData(data) {
 
-    const response = await fetch(API, {
+    await fetch(API, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -86,7 +85,7 @@ async function createData(data) {
             <h1 class="title">${data.name} ${data.lastName}</h1>
             <p class="date">${realDate}</p>
             <span class="mark">${countMark(data.mark)}</span>
-            <p class="status" id="status">${countMark(data.mark) > 40 ? 'Pass' : 'Fail'}</p>
+            <p class="status" id="status">${data.mark > 60 ? 'Pass' : 'Fail'}</p>
             <button class="edit" onclick="editData(${data.id})">
                 <i class="fa-solid fa-pen"></i>
             </button>
@@ -110,11 +109,12 @@ _addForm.addEventListener('submit', (e) => {
     let newData = {
         name: name.value,
         lastName: lastname.value,
-        markedDate: realDate,
+        markDate: realDate,
         mark: mark.value,
     }
     createData(newData)
     
+    _addForm.reset()
     _addDataModal.style.display = 'none'
     document.querySelector('.modal-backdrop').style.display = 'none'
 })
@@ -129,17 +129,51 @@ async function deleteData(id) {
             method: 'DELETE',
         });
         
-        addData(await data())
+        await data()
     } catch (error) {
         alert(error.message);
     }
 }
 
+
 // edit
-
-
-async function editData(id) {
+async function editData(id, itemId) {
     _editModal.style.display = 'block'
+
+    let {edit_name, edit_lName, edit_mark} = _editForm.elements
+
+    let name = data().then((data) => data[itemId].name)
+    let lastName = data().then((data) => data[itemId].lastName)
+    let mark = data().then((data) => data[itemId].mark)
+
+
+    edit_name.value = `${await name}`
+    edit_lName.value = `${await lastName}`
+    edit_mark.value = `${await mark}`
+
+    _editForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        _editModal.style.display = 'none'
+        
+
+        const response = await fetch(API + `/${id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: edit_name.value.trim(),
+                lastName: edit_lName.value.trim(),
+                mark: edit_mark.value.trim()
+            })
+        })
+        if(await response){
+            data(API)
+        }else{
+            alert("xatolik editda")
+        }
+
+    })
 }
 
 _closeBtn.addEventListener('click', () =>{
